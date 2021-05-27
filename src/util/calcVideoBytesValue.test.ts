ce4812bbfa0962b813f475befe8bdf6274cdf844
Hibikine Kage <goodsmile314159@gmail.com>
2021-05-28 05:04:58 +0900

    add tests

--- src/util/calcVideoBytesValue.test.ts
+++ src/util/calcVideoBytesValue.test.ts
@@ -1,12 +1,10 @@
 import { ByteType, calcValue } from './calcVideoBytesValue';
 
-test.each([[1024, 'KB', '1:00', 'KB', (1024 / 60) * 8]] as [
-  number,
-  ByteType,
-  string,
-  ByteType,
-  number
-][])(
+test.each([
+  [1024, 'KB', '1:00', 'KB', (1024 / 60) * 8],
+  [1024, 'KB', '1', 'KB', 8192],
+  [10, 'MB', '1:00:00', 'KB', 10*1000*8/3600],
+] as [number, ByteType, string, ByteType, number][])(
   'calcValue(%i,%s,%s,%s)',
   (bytes, byteType, len, outByteType, expected) => {
     expect(calcValue(bytes, byteType, len, outByteType)).toBe(expected);
